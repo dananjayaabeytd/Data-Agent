1c0a171e9736c4b6c99f065eb1eb49385f1980ab
Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2026-08-31 21:49:25 +0530

    feat: implement Data Agent graph with routing logic and main entry point

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_core.messages import HumanMessage
+
+from agents.data_agent import data_agent
+
+if __name__ == "__main__":
+    response = data_agent.invoke(
+        {"messages":[HumanMessage(content="I want to extract the data from the API endpoint 'https://pokeapi.co/api/v2/pokemon' and save it to data/extract folder in the csv folder")],
+         "route_response": ""}
+    )
+
+    print(response)
